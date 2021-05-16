a26639cc0e221d97f8d8c5a24b9a7233b76893e0
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-16 08:41:45 +0600

    crud views urls

--- postyourmind/blog/models.py
+++ postyourmind/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=256)
     text = models.TextField()
-    current_time = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
